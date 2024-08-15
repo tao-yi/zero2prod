66e52e395f0635df8b59e82bd51778edc4bbb9e7
taoyi <ytao6122@uni.sydney.edu.au>
2024-08-15 21:25:09 +0800

    fix init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -16,9 +16,9 @@ if ! [ -x "$(command -v sqlx)" ]; then
 fi
 
 # Check if a custom user has been set, otherwise default to 'postgres'
-DB_USER=${POSTGRES_USER:=postgres}
+DB_USER=${POSTGRES_USER:=admin}
 # Check if a custom password has been set, otherwise default to 'password'
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=admin}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
